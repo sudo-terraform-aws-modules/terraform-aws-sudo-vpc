56be447f0841eb2b95b85b27fba2a1ba6fd3c98f
Dipendra Chaudhary <dipendrac3@gmail.com>
2023-07-25 20:44:54 +0100

    vpc dhcp options added

--- main.tf
+++ main.tf
@@ -58,8 +58,8 @@ resource "aws_vpc" "this" {
   instance_tenancy               = var.instance_tenancy
   enable_dns_hostnames           = var.enable_dns_hostnames
   enable_dns_support             = var.enable_dns_support
-  enable_classiclink             = null # https://github.com/hashicorp/terraform/issues/31730
-  enable_classiclink_dns_support = null # https://github.com/hashicorp/terraform/issues/31730
+  enable_classiclink             = null
+  enable_classiclink_dns_support = null
 
   tags = merge(
     { "Name" = var.name },
@@ -75,6 +75,33 @@ resource "aws_vpc_ipv4_cidr_block_association" "this" {
   vpc_id = aws_vpc.this[0].id
 
   cidr_block = element(var.secondary_cidr_blocks, count.index)
+}
+
+################################################################################
+# DHCP Options Set
+################################################################################
+
+resource "aws_vpc_dhcp_options" "this" {
+  count = local.create_vpc && var.enable_dhcp_options ? 1 : 0
+
+  domain_name          = var.dhcp_options_domain_name
+  domain_name_servers  = var.dhcp_options_domain_name_servers
+  ntp_servers          = var.dhcp_options_ntp_servers
+  netbios_name_servers = var.dhcp_options_netbios_name_servers
+  netbios_node_type    = var.dhcp_options_netbios_node_type
+
+  tags = merge(
+    { "Name" = var.name },
+    var.tags,
+    var.dhcp_options_tags,
+  )
+}
+
+resource "aws_vpc_dhcp_options_association" "this" {
+  count = local.create_vpc && var.enable_dhcp_options ? 1 : 0
+
+  vpc_id          = local.vpc_id
+  dhcp_options_id = aws_vpc_dhcp_options.this[0].id
 }
 
 ################################################################################
--- variables.tf
+++ variables.tf
@@ -289,3 +289,45 @@ variable "default_security_group_tags" {
   type        = map(string)
   default     = {}
 }
+
+variable "enable_dhcp_options" {
+  description = "Should be true if you want to specify a DHCP options set with a custom domain name, DNS servers, NTP servers, netbios servers, and/or netbios server type"
+  type        = bool
+  default     = false
+}
+
+variable "dhcp_options_domain_name" {
+  description = "Specifies DNS name for DHCP options set (requires enable_dhcp_options set to true)"
+  type        = string
+  default     = ""
+}
+
+variable "dhcp_options_domain_name_servers" {
+  description = "Specify a list of DNS server addresses for DHCP options set, default to AWS provided (requires enable_dhcp_options set to true)"
+  type        = list(string)
+  default     = ["AmazonProvidedDNS"]
+}
+
+variable "dhcp_options_ntp_servers" {
+  description = "Specify a list of NTP servers for DHCP options set (requires enable_dhcp_options set to true)"
+  type        = list(string)
+  default     = []
+}
+
+variable "dhcp_options_netbios_name_servers" {
+  description = "Specify a list of netbios servers for DHCP options set (requires enable_dhcp_options set to true)"
+  type        = list(string)
+  default     = []
+}
+
+variable "dhcp_options_netbios_node_type" {
+  description = "Specify netbios node_type for DHCP options set (requires enable_dhcp_options set to true)"
+  type        = string
+  default     = ""
+}
+
+variable "dhcp_options_tags" {
+  description = "Additional tags for the DHCP option set (requires enable_dhcp_options set to true)"
+  type        = map(string)
+  default     = {}
+}
